28057786fe5f6e09e1e60857d38deb9ff52a8f55
Anton <imladris.doc@gmail.com>
2023-06-19 18:59:49 +0300

    2

---
+++ app/src/main/java/com/example/Constant.kt
@@ -0,0 +1,5 @@
+package com.example
+
+import com.example.infobets.MainActivity
+
+lateinit var MAIN: MainActivity
--- app/src/main/java/com/example/infobets/MainActivity.kt
+++ app/src/main/java/com/example/infobets/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.infobets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.MAIN
+import com.example.infobets.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        MAIN = this
     }
 }
